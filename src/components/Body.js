f1993191434dd6b87b2a40b20f8cccbbcedfff04
muppalajeethu <muppalajeethu20@gmail.com>
2025-02-10 02:01:54 +0530

    latest update

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,32 @@
 import Restaurant from "./Restaurant";
 import resList from "../utils/mockdata";
+import { useState } from "react";
 
 const Body = () => {
+  const [ListofRestarurant, setListofRestarurant] = useState(resList);
+
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button
+          className="filter-btn"
+          onClick={() => {
+            const filtered = ListofRestarurant.filter(
+              (res) => res?.info?.avgRating > 4 
+            );
+            setListofRestarurant(filtered);
+          }}
+        >
+          Top Rated Restaurants
+        </button>
+      </div>
       <div className="res-container">
-        {resList.map((restaurant) => (
+        {ListofRestarurant.map((restaurant) => (
           <Restaurant key={restaurant.info.id} resData={restaurant} />
         ))}
       </div>
     </div>
   );
 };
+
 export default Body;
